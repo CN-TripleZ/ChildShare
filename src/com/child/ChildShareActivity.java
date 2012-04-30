f399277c65f9f07d623620f480d4ef9a97c0e53f
jeffreyzhang <shine8606@gmail.com>
2012-05-01 03:04:47 +0800

    modify

--- src/com/child/ChildShareActivity.java
+++ src/com/child/ChildShareActivity.java
@@ -9,6 +9,6 @@ public class ChildShareActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///assets/html/index.html");
+        super.loadUrl("file:///android_asset/html/index.html");
     }
 }
