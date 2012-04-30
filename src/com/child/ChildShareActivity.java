b0c3af1665beb098262e9d9f32aec8b378d60761
jeffreyzhang <shine8606@gmail.com>
2012-05-01 02:49:40 +0800

    new project

---
+++ src/com/child/ChildShareActivity.java
@@ -0,0 +1,14 @@
+package com.child;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class ChildShareActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///assets/html/index.html");
+    }
+}
